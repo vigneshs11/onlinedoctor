466ffb459765c3c72a9370777d6eb995178ab448
Vignesh <vigs@lowes.com>
2019-11-11 11:56:47 +0530

    first commit

---
+++ src/main/java/com/lowes/healthcare/onlineDoctor/OnlineDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.lowes.healthcare.onlineDoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineDoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lowes/healthcare/onlineDoctor/OnlineDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lowes.healthcare.onlineDoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
